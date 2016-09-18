285dccd55168f0b4d941416a307d2f1c5e8faad4
EchoWine <dimitri.who@gmail.com>
2016-09-18 16:41:21 +0200

    First commit

---
+++ src/Debug.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace CoreWine\Component;
+
+/**
+ * A helper class for debugging.
+ */
+class Debug{
+
+	public static $data;
+
+	/**
+	 * Add to stack.
+	 *
+	 * @param DataType $data
+	 */
+	public static function add($data){
+		Debug::$data[] = $data;
+	}
+
+	/**
+	 * Print the stack.
+	 */
+	public static function print(){
+		print_r(Debug::$data);
+	}
+}
+
+?>
