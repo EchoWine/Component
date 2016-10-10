f6b024f8348a3367d3e4f860772a1a2a88c006d6
EchoWine <dimitri.who@gmail.com>
2016-10-10 02:23:38 +0200

    Improved Collection

--- src/Collection.php
+++ src/Collection.php
@@ -8,9 +8,24 @@ use Countable;
 
 class Collection extends \ArrayObject{
 
-
     public function has($value){
         return in_array($value,$this -> container);
+    }
+
+    public function addParam($name,$value){
+
+    	$v = $this;
+    	foreach($v as $n => $k){
+    		if(is_object($k)){
+    			$k -> {$name} = $value;
+    		}else{
+    			$v[$n][$name] = $value; 
+    		}
+    	}
+    }
+
+    public function merge($array){
+    	return new Collection(array_merge((array)$this,(array)$array));
     }
     
 }
