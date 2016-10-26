d48504470ff032198d0893bc0f0ee39017c6328f
EchoWine <dimitri.who@gmail.com>
2016-10-26 20:37:03 +0200

    Added Exception

---
+++ src/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CoreWine\Component;
+
+class Exception extends \Exception{
+
+	/**
+	 * A list of useful information about the exception
+	 * 
+	 * @var array
+	 */
+	public $info = [];
+
+}
